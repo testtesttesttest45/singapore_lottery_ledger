8a59c78e4f24b3d7bae0b9ca7d5cae76d08453d6
testtesttesttest45 <weilinquek201@gmail.com>
2023-10-24 20:25:15 +0800

    testing again

--- public/js/main2.js
+++ public/js/main2.js
@@ -1531,9 +1531,9 @@ document.addEventListener('DOMContentLoaded', function () {
     
     function formatDateToLocalDateString(dateString) {
         const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JS
-        const day = String(date.getDate()).padStart(2, '0');
+        const year = date.getUTCFullYear();
+        const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are 0-based in JS
+        const day = String(date.getUTCDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
     
